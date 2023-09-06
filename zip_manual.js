17685d344c1ba66cae9a04c4f9fea9ad7478f49e
Trần Kim Tín <tintk62@wru.vn>
2023-09-06 15:02:56 +0700

    Create zip_manual.js

---
+++ zip_manual.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const zip = require('./zip');
+let symbols = ['BTCBUSD', 'BTCUSDT', 'BTCTUSD'];
+let date = '2023-09-03';
+let zipPath = `${__dirname}/data/${date}.zip`;
+let fileList = symbols.map(item => `${item}_${date}.txt`);
+console.log(zipPath, fileList)
+zip(fileList, zipPath, false);
