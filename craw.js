dad5f2ee35f3e773211245a14d8436e174c5cd3f
Trần Kim Tín <tintk62@wru.vn>
2023-10-17 17:46:56 +0700

    a

--- craw.js
+++ craw.js
@@ -5,7 +5,7 @@ const moment = require('moment');
 const zip = require('./zip');
 // const client = Binance();
 
-let symbols = ['BTCBUSD', 'BTCUSDT', 'BTCTUSD'];
+let symbols = ['BTCUSDT', 'ETHUSDT', 'f_BTCUSDT', 'f_ETHUSDT'];
 let lines = {};
 let block = 1000;
 let lastDate = moment().utc(0).format('YYYY-MM-DD');
@@ -16,11 +16,13 @@ for (let symbol of symbols) {
 let ziped = {};
 async function main(s) {
     console.log('craw', s, moment().format('YYYY-MM-DD HH:mm:SS'));
-    const trade = new WebSocket(`wss://stream.binance.com:9443/ws/${s}@trade`);
+    const trade = new WebSocket(`wss://${(s.includes('f_') ? 'f' : '')}stream.binance.com/ws/${s.replace('f_', '')}@trade`);
     trade.on('message', function incoming(data) {
         try {
             data = JSON.parse(data.toString());
             let symbol = data.s;
+            let isFuture = data.X ? true : false;
+            if (isFuture) symbol = 'f_' + symbol;
             let date = moment(data.E).utc(0).format('YYYY-MM-DD');
             if (date != lastDate && !ziped[lastDate]) {
                 console.log({ date, lastDate })
@@ -30,7 +32,7 @@ async function main(s) {
                 let zipPath = `${__dirname}/data/${_lastDate}.zip`;
                 let fileList = symbols.map(item => `${item}_${_lastDate}.txt`);
                 for (let _symbol of symbols) {
-                    let s = lines[symbol].join('\n') + '\n';
+                    let s = lines[_symbol].join('\n') + '\n';
                     lines[_symbol] = [];
                     fs.appendFileSync(`${__dirname}/${_symbol}_${_lastDate}.txt`, s);
                 }
--- test.js
+++ test.js
@@ -1,21 +1,6 @@
-var ping = require('ping');
-const fs = require('fs');
-
-let hosts = fs.readdirSync('C:\\Users\\DreamStore\\Desktop\\Tin_remote_shortcut');
-hosts = hosts.map(item => {
-    let dotIndex = item.indexOf('.');
-    item = item.slice(dotIndex - 3, dotIndex + 12)
-        .split('')
-        .filter(item => item == '.' || (item >= '0' && item <= '9'));
-    if (item[item.length - 1] == '.') item.pop();
-    return item.join('');
-});
-hosts = [...new Set(hosts)];
-
-async function main() {
-    for (let host of hosts) {
-        let res = await ping.promise.probe(host);
-        console.log(res.host, res.alive);
-    }
-}
-main();
+const WebSocket = require('ws');
+const trade = new WebSocket(`wss://stream.binance.com/ws/btcusdt@trade`);
+let cnt = 0;
+trade.on('message', function incoming(data) {
+    console.log(cnt++, data.toString());
+})
